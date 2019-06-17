832d67467ff2b95216b4e15e349990e9a1afc97c
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2019-06-17 12:59:53 +0900

    added pip upgrade

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # setup awscli, parallelcluster on CentOS7
 
-sudo yum install -y wget epel-release centos-release-scl python-pip zstd bzip2 zip unzip jq tree net-tools rsync traceroute telnet
+sudo yum install -y epel-release centos-release-scl
+sudo yum install -y wget python-pip zstd bzip2 zip unzip jq tree net-tools rsync traceroute telnet
 sudo yum groupinstall -y "Development tools"
-sudo pip install awscli
-sudo pip install aws-parallelcluster
+sudo pip install -U pip
+sudo pip install awscli aws-parallelcluster
